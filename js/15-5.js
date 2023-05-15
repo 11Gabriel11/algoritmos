4f1ee636d4cca11dd59635145f98f0acf8443f49
11Gabriel11 <nahuel.colonia@gmail.com>
2023-05-15 17:30:48 -0300

    15/5
    15/5

---
+++ js/15-5.js
@@ -0,0 +1,13 @@
+let num = 545
+function ejemplo(params) {
+    let inp = document.querySelector("#entrada");
+    let p = document.querySelector("#salida");
+    let divImg = document.querySelector("#img");
+    divImg.innerHTML = '<img src="../img/descarga.jfif" alt="error"/>';
+    let valor = inp.value;
+    p.innerText = valor;
+    console.log(p.value);
+    console.log(valor);
+    console.dir(inp);
+    console.log(num);
+}
