47071fe73bc1f9b94c7ae7022719dd3a9eb7b50f
JoaquinReyes2905 <Mikemoble2905@gmail.com>
2023-05-29 15:16:04 -0300

    tarea
    29/5

--- js/tarea.js
+++ js/tarea.js
@@ -1,5 +1,7 @@
+function meses (){
 let meses = ["Enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
 
 for(let i = 0; i < meses.length; i++) {
     console.log(meses[i]);
+}
 }
