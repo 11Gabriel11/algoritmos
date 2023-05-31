3821c278c2c1679bd4318229059df69aab68298e
11Gabriel11 <nahuel.colonia@gmail.com>
2023-05-31 17:10:49 -0300

    31/5

--- js/tarea.js
+++ js/tarea.js
@@ -1,7 +1,38 @@
 function meses (){
-let meses = ["Enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+    let meses = ["Enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+    
+    for(let i = 0; i < meses.length; i++) {
+        console.log(meses[i]);
+    }
+    }
+    
+//2
+function Ejercicio2(){
+    var valores = [true, 5, false, "hola", "adios",2,"gjdjdflk"];
+    var valores = 0;
+    var posicion= "";
 
-for(let i = 0; i < meses.length; i++) {
-    console.log(meses[i]);
+    for (let i = 0; i < valores.length; i++){
+        if (typeof(valores[i]) == "string"){
+            if (aux[i].length > aux[i+1].length && i+1 < aux.length) {
+                posicion = i;
+                valor = aux[i];
+            } else {
+                posicion = i +1;
+                valor = aux[i + 1];
+            }
+            }
+        }
+    }
+
+    console.log("el mayor es "+valor);
+
+    //comparacion directa
+if (valores[3].length > valores[4].length) {
+    console.log("hola es mayor");
+}else{
+    console.log("adios es mayor")
 }
-}
+
+
+
