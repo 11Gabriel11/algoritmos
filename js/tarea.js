7dec2de9a987c036100faf02cd3eba3f480f877b
11Gabriel11 <nahuel.colonia@gmail.com>
2023-06-12 17:39:50 -0300

    6_12

--- js/tarea.js
+++ js/tarea.js
@@ -1,3 +1,4 @@
+//1
 function meses (){
     let meses = ["Enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
     
@@ -34,5 +35,80 @@ if (valores[3].length > valores[4].length) {
     console.log("adios es mayor")
 }
 
+//3
+function Ejercicio3() {
+var numero1 = 5;
+var numero2 = 8;
+if(numero1 < numero2) {
+console.log("numero1 no es mayor que numero2");
+}
+if(numero2 > 0) {
+console.log("numero2 es positivo");
+}
+if(numero1 < 0 || numero1 != 0) {
+console.log("numero1 es negativo o distinto de cero");
+}
+numero1 = numero1 + 1;
+if(numero2 > numero1) {
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+}
 
 
+
+//6
+function determinarParidad(numero) {
+    if (numero % 2 === 0) {
+      return "El número es par.";
+    } else {
+      return "El número es impar.";
+    }
+  }
+  
+  var numero = 7;
+  var resultado = determinarParidad(numero);
+  console.log(resultado);
+
+
+  //7
+  function determinarTipoCadena(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+      return "La cadena está formada solo por mayúsculas.";
+    } else if (cadena === cadena.toLowerCase()) {
+      return "La cadena está formada solo por minúsculas.";
+    } else {
+      return "La cadena está formada por una mezcla de mayúsculas y minúsculas.";
+    }
+  }
+  
+  var texto = "Hola Mundo";
+  var resultado = determinarTipoCadena(texto);
+  console.log(resultado);
+
+
+  //8
+  function esPalindromo(cadena) {
+    // Eliminamos los espacios en blanco y convertimos todo a minúsculas
+    var cadenaSinEspacios = cadena.replace(/\s/g, "").toLowerCase();
+    
+    // Invertimos la cadena
+    var cadenaInvertida = cadenaSinEspacios.split("").reverse().join("");
+    
+    // Comparamos la cadena original con la cadena invertida
+    if (cadenaSinEspacios === cadenaInvertida) {
+      return "La cadena es un palíndromo.";
+    } else {
+      return "La cadena no es un palíndromo.";
+    }
+  }
+  
+  var texto = "La ruta nos aporto otro paso natural";
+  var resultado = esPalindromo(texto);
+  console.log(resultado);
+
+
+
+  //9
+  
+  
+  
